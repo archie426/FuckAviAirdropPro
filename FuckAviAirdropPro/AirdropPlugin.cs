b59d2488a821d0e47b38bd549c5f12c0ee1137e7
DefCon42 <klrehm123@gmail.com>
2018-01-11 06:03:15 -0500

    progress has been made

---
+++ FuckAviAirdropPro/AirdropPlugin.cs
@@ -0,0 +1,36 @@
+﻿using Rocket.API.Collections;
+using Rocket.Core.Plugins;
+
+namespace FuckAviAirdropPro
+{
+    public class AirdropPlugin : RocketPlugin<AirdropConfig>
+    {
+        public static AirdropPlugin Instance;
+        public static AirdropConfig Conf;
+        public AirdropManager ManagerInstance;
+        
+        protected override void Load()
+        {
+            Instance = this;
+            Conf = Configuration.Instance;
+
+            ManagerInstance = gameObject.AddComponent<AirdropManager>();
+        }
+
+        protected override void Unload()
+        {
+           Destroy(ManagerInstance);
+        }
+        
+        public override TranslationList DefaultTranslations => new TranslationList()
+        {
+            {"airdrop_land", "The airdrop has landed at {0}!"},
+            {"vehicle_land", "The vehicle has landed at {0}!"},
+            
+            {"airdrop_launch", "An airdrop has been launched at {0}!"},
+            {"random_airdrop_launch", "An airdrop has been launched at a random location!"},
+            {"vehicle_launch", "A vehicle has been launched at {0}!"},
+            {"random_vehicle_launch", "A vehiclehas been launched at a random location!"}
+        };
+    }
+}
---
+++ FuckAviAirdropPro/CustomAirdrop.cs
@@ -0,0 +1,40 @@
+﻿using SDG.Unturned;
+using UnityEngine;
+
+namespace FuckAviAirdropPro
+{
+    public class CustomAirdrop : MonoBehaviour
+    {
+        private bool isExploded;
+        public AirdropType type;
+        
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (isExploded)
+                return;
+            
+            if (collision.collider.isTrigger)
+                return;
+            
+            isExploded = true;
+            
+            Transform t = BarricadeManager.dropBarricade(new Barricade(1374), null, base.transform.position, 0f, 0f, 0f, 0UL, 0UL);
+            
+            if (t == null) 
+                return;
+            
+            InteractableStorage component = transform.GetComponent<InteractableStorage>();
+            component.despawnWhenDestroyed = true;
+            if (component.items != null)
+            {
+                foreach (AirdropItem ai in AirdropManager.GetAirdropItems(type))
+                    for (int i = 0; i < ai.amount; i++)
+                        component.items.tryAddItem(new Item((ushort) ai.id, EItemOrigin.ADMIN, (byte) ai.durability), false);
+                
+                component.items.onStateUpdated();
+            }
+            
+            EffectManager.sendEffectReliable((ushort)AirdropPlugin.Conf.AirdropEffectId, EffectManager.INSANE, transform.position);
+        }
+    }
+}
